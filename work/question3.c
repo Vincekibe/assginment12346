c97b1deb5a175a40098242d6c91816e04ebd85d2
Vincekibe <kibetvincent536@gmail.com>
2024-10-03 05:08:58 -0700

    Update question3.c

--- work/question3.c
+++ work/question3.c
@@ -1,21 +1,24 @@
 //calculate age to be given a loan.
-
+/*NAME:VINCENT KIBET
+REG:CT101/G/24767/24 */
 #include <stdio.h>
 
 int main(){
 
 int age=21;
-int income=21000;
+int montlyincome=21000;
 
    printf("Enter your age:");
    scanf("%d", &age);
+   Printf("Enter your monthly income");
+   scanf("%d", &montlyincome);
 
 if(age>=21){
   printf("Congratulation you qualify a loan");
 
 }
 
-if(age<21){
+else{
    printf("Unfortunately,we are unable to offer you a loan at this time.");
 
 }
